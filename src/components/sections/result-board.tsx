0941d1db3e02a72d7eada9472f9c8cd79558db96
AlexandrTurov <alexandr.turov@mail.ru>
2024-12-19 23:01:21 +0700

    make ResultBoard component

---
+++ src/components/sections/result-board.tsx
@@ -0,0 +1,12 @@
+import Section from "./section";
+
+const ResultBoard = () => {
+  return (
+    <Section>
+      <>Результаты</>
+      <>Доска резутьтатов</>
+    </Section>
+  );
+};
+
+export default ResultBoard;
